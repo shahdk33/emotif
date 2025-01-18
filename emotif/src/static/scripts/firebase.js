815215e6f4e4a735d3f4a407e520259b696ddc7c
shahdk33 <sh977589@dal.ca>
2025-01-18 16:32:46 -0400

    firebase config aditions

--- emotif/src/static/scripts/firebase.js
+++ emotif/src/static/scripts/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Export the database and helper functions
+export { database, ref, set };
