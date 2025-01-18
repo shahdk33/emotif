41dfe7e9e416318bb9276099ff3691509e044573
sharma4 <sh319698@dal.ca>
2025-01-18 17:10:03 -0400

    trying firebase

--- emotif/src/static/scripts/firebase.js
+++ emotif/src/static/scripts/firebase.js
@@ -1,24 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, child, get } from "firebase/database";
 
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyBLxfABkXzNfnu2Yw8xMxKsvHjZAPhdhtM",
+  authDomain: "emotif-221b2.firebaseapp.com",
+  databaseURL: "https://emotif-221b2-default-rtdb.firebaseio.com",
+  projectId: "emotif-221b2",
+  storageBucket: "emotif-221b2.firebasestorage.app",
+  messagingSenderId: "160120301390",
+  appId: "1:160120301390:web:741e1311224188f8baa01f"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
-// Export the database and helper functions
-export { database, ref, set };
+const dbRef = ref(getDatabase());
+get(child(dbRef, `events/`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
